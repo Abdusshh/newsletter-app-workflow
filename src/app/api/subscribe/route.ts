12e328d4bda34f337c4b0ff0e3bf0ba16c8af90d
Abdusshh <abdullah.gules@simpplr.com>
2024-09-24 09:49:25 +0300

    Actual implementation of subscribe route

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,5 +1,5 @@
 // import { NextRequest, NextResponse } from "next/server";
-// import { redis } from "@/lib/redis";
+import { redis } from "@/lib/redis";
 import { SubscriptionData } from "@/types";
 import { serve } from "@upstash/qstash/nextjs";
 
@@ -42,8 +42,10 @@ export const POST = serve<SubscriptionData>(async (context) => {
     frequency = "30";
   }
 
-  // Validate frequency is a number
+  // We will use this number to calculate the time to wait for the next email
   const frequencyNumber = Number(frequency);
+
+  // Validate frequency is a number
   if (isNaN(frequencyNumber) || frequencyNumber <= 0) {
     console.error(
       "Frequency must either be daily, weekly, monthly, or a positive number for custom number of days."
@@ -53,10 +55,8 @@ export const POST = serve<SubscriptionData>(async (context) => {
 
   // Check if the email is already subscribed
   const exists = await context.run("check-user-subscription", async () => {
-    // Add Redis check logic here
     console.log("Checking if user is already subscribed");
-    return false;
-    //   return await redis.exists(`user:${email}`);
+    return await checkSubscription(email);
   });
 
   if (exists) {
@@ -66,29 +66,101 @@ export const POST = serve<SubscriptionData>(async (context) => {
 
   // Store the subscription in Redis
   await context.run("add-user-to-redis", async () => {
-    // Add to Redis logic here
     console.log("Adding user to Redis");
-    //   await redis.set(
-    //     `user:${email}`,
-    //     JSON.stringify({ frequency: frequencyNumber })
-    //   );
+    await redis.set(
+      `user:${email}`,
+      JSON.stringify({ frequency: frequencyNumber })
+    );
   });
 
   // Enqueue the workflow to send an email (using QStash)
   await context.run("send-email", async () => {
     console.log("Sending welcome email to", email);
-    // Add logic to send a welcome email here
-    await sendEmail("Welcome to the platform", email);
+    await sendEmail("Welcome to Upstash Newsletter!", email);
   });
 
   console.log("Subscription successful!");
+
+  // We will send at most 5 newsletters so that we will not run the workflow forever in this example
+  let newsletterCount = 5; // Number of newsletters to send
+
+  while (true) {
+    // Check if we have sent enough newsletters
+    if (newsletterCount === 0) {
+      console.log("Sent desired number of newsletters. Stopping the workflow.");
+      break;
+    }
+
+    // Wait for the frequency to send the next email
+    await context.sleep(
+      "wait-for-user-frequency",
+      60 * 60 * 24 * frequencyNumber
+    );
+
+    // Check if the user is still subscribed
+    const exists = await context.run("check-user-subscription", async () => {
+      console.log("Checking if user is still subscribed");
+      return await checkSubscription(email);
+    });
+
+    if (!exists) {
+      console.log("User is not subscribed anymore. Stopping the workflow.");
+      break;
+    }
+
+    // Send the newsletter email
+    await context.run("send-email", async () => {
+      console.log("Sending newsletter email to", email);
+      await sendEmail(`
+        Here is your daily newsletter!
+        You are receiving this email because you subscribed to Upstash Newsletter.
+        You can unsubscribe anytime by clicking the link in the email.
+        You have ${newsletterCount} newsletters left.
+        <LINK TO UNSUBSCRIBE>
+        `,
+        email
+      );
+    });
+
+    newsletterCount--;
+  }
+
   return;
 });
 
-// Example function for sending email
+// Function for sending email
 async function sendEmail(message: string, email: string) {
-  // Implement email sending logic here
   console.log(`Sending ${message} email to ${email}`);
+  const url = process.env.EMAIL_SERVICE_URL;
+  const payload = {
+    to_email: email,
+    subject: "Upstash Newsletter",
+    content: message,
+  };
+
+  if (!url) {
+    console.error("EMAIL_SERVICE_URL is not defined.");
+    return;
+  }
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    console.error("Failed to send email:", await response.text());
+  }
+}
+
+// function for checking if the user is still subscribed
+async function checkSubscription(email: string) {
+  const exists = await redis.exists(`user:${email}`);
+    console.log("User is subscribed:", exists);
+  return exists;
 }
 
 // export async function POST(request: NextRequest) {
